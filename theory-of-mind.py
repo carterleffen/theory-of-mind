845fd14917108c623905b7386f3d820daa58e2a3
Carter Leffen <cleffen@gmail.com>
2023-05-02 11:47:30 -0400

    Update theory-of-mind.py
    Improved the splitting of input.

--- theory-of-mind.py
+++ theory-of-mind.py
@@ -1,13 +1,9 @@
-"""
-This script generates unique scenarios, questions, and answers based on user input. It
-uses OpenAI's GPT-3 model to generate text and the dotenv library to manage environment variables.
-"""
-
 import os
 import re
-import json
 import openai
 from dotenv import load_dotenv
+import csv
+from io import StringIO
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -15,8 +11,22 @@ load_dotenv()
 # Constants
 API_KEY = os.environ.get("OPENAI_API_KEY")
 MODEL_NAME = "gpt-3.5-turbo"
-temperature: 0.4
+temperature = 0.4
+max_tokens = 2000
 openai.api_key = API_KEY
+
+def csv_to_dict(csv_string):
+    """Convert a CSV string into a dictionary."""
+    reader = csv.DictReader(StringIO(csv_string), delimiter='|')
+    return next(reader)
+
+def dict_to_csv(data_dict):
+    """Convert a dictionary into a CSV string."""
+    output = StringIO()
+    writer = csv.DictWriter(output, fieldnames=data_dict.keys(), delimiter='|')
+    writer.writeheader()
+    writer.writerow(data_dict)
+    return output.getvalue().strip()
 
 class InputValidator:
     """
@@ -55,22 +65,14 @@ class APIKeyHandler:
         return True
 
 class ScenarioGenerator:
-    """
-    Class to generate scenarios, questions, and answers. Uses OpenAI's GPT-3 model to generate text.
-    """
     def __init__(self, user_scenario, user_question, iteration):
-        """Initialize ScenarioGenerator with a user scenario, user question, and iteration number."""
         self.user_scenario = user_scenario
         self.user_question = user_question
         self.iteration = iteration
 
     def generate(self):
-        """
-        Generate a scenario, question, and answer. Returns a dictionary containing the scenario,
-        question, and answer. If an error occurs during generation, returns None.
-        """
         messages = [
-            {"role": "system", "content": "You are an expert in problem solving and critical thinking. Use your skills, thinking step by step to answer the last question, taking inspiration from the previous scenarios. All output you generate is in JSON format."},
+            {"role": "system", "content": "You are an expert in problem solving and critical thinking. Use your skills, thinking step by step to answer the last question, taking inspiration from the previous scenarios. All output you generate is in CSV format."},
             {"role": "user", "content": f"Given the scenario \"{self.user_scenario}\" and \"{self.user_question}\", generate a completely random, unrelated scenario with its corresponding question and answer in iteration {self.iteration}. Please format your response as follows:\n\nScenario: ...\nQuestion: ...\nAnswer: ...\n\nAnd separate the scenario, question, and answer by new lines."}
         ]
 
@@ -82,7 +84,6 @@ class ScenarioGenerator:
 
         generated_text = response['choices'][0]['message']['content'].strip()
 
-        # Split the generated text based on different headers
         sections = re.split(r"\n(?:Scenario: |Question: |Answer: )", generated_text)
         sections = [section.strip() for section in sections if section.strip()]
 
@@ -100,28 +101,23 @@ class ScenarioGenerator:
         return scenario_data
 
 def chat_completion(messages):
-    """Function to call OpenAI API for a chat completion task."""
     response = openai.ChatCompletion.create(
         model=MODEL_NAME,
         messages=messages,
-        temperature=0.4,
-        max_tokens=2000,
+        temperature=temperature,
+        max_tokens=max_tokens,
     )
     return response
 
 def split_input(input_text):
-    """
-    Split the user input into a scenario and a question.
-
-    Args:
-        input_text (str): The user input.
-
-    Returns:
-        tuple: The scenario and question, or (None, None) if an error occurs.
-    """
     messages = [
-        {"role": "system", "content": "You are an expert in text analysis. Split the following text into a scenario and a question in JSON format."},
-        {"role": "user", "content": input_text}
+    {
+        "role": "system",
+        "content": """You are an expert in semantic understanding and analysis. You know exactly where the true question lies. You are an expert in logic and reason. Thinking step by step. 1. Extract [Question] from [Text]. 2. Add [Text] without [Question] to [Scenario]. 3. Output as follows:
+        Scenario: [Scenario]
+        Question: [Question]""",
+    },
+    {"role": "user", "content": input_text},
     ]
 
     try:
@@ -130,16 +126,21 @@ def split_input(input_text):
         print(f"Error while splitting input: {error}")
         return None, None
 
-    json_text = response['choices'][0]['message']['content'].strip()
+    generated_text = response['choices'][0]['message']['content'].strip()
 
-    try:
-        scenario_data = json.loads(json_text)
-    except json.JSONDecodeError:
-        print("Error decoding JSON. Please try again.")
+    # Split the generated text based on different headers
+    sections = re.split(r"\n(?:Scenario: |Question: )", generated_text)
+    sections = [section.strip() for section in sections if section.strip()]
+
+    if len(sections) < 2:
+        print("Unexpected response from the model. It did not generate a complete scenario and question. Please try again.")
+        print(f"Generated text: {generated_text}")  # For debugging purposes
         return None, None
 
-    return scenario_data.get("scenario"), scenario_data.get("question")
+    scenario = sections[0].replace("Scenario: ", "", 1)
+    question = sections[1].replace("Question: ", "", 1)
 
+    return scenario, question
 
 def process_answer(answer):
     """
@@ -168,23 +169,14 @@ def display_scenarios(scenarios):
         scenarios (list): A list of scenarios to display.
     """
     for i, scenario in enumerate(scenarios, start=1):
-        print(f"\nScenario {i}:")
+        print(f"\nAuto Generated Scenario {i}:")
         print(f"Scenario: \"{scenario['scenario']}\"")
         print(f"\nQ: {scenario['question']}")
         print(f"\nA: {scenario['answer']}\n")
 
 def generate_answer(prompt):
-    """
-    Generate an answer based on the given prompt.
-
-    Args:
-        prompt (str): The prompt to pass to the API.
-
-    Returns:
-        str: The generated answer, or None if an error occurs.
-    """
     messages = [
-        {"role": "system", "content": "You are an expert in problem solving and critical thinking. Use your skills, thinking step by step to answer the last question, taking inspiration from the previous scenarios. You output everything in JSON format."},
+        {"role": "system", "content": "You are an expert in problem solving and critical thinking. Use your skills, thinking step by step to answer the last question, taking inspiration from the previous scenarios. You output everything in CSV format."},
         {"role": "user", "content": prompt}
     ]
 
@@ -198,12 +190,13 @@ def generate_answer(prompt):
     return answer
 
 def main():
-    """
-    Main function to generate and display scenarios based on user input. 
-    Handles user input, input validation, scenario generation, and output display.
-    """
-    user_input = input("\nPlease enter a scenario and a question: ")
+    print("Theory of Mind + Thinking Step By Step - Proof of Concept\n")
+    print("Provide a scenario and question you want answered.\n")
+    print("Example: The morning of the high school dance Sarah placed her high heel shoes under her dress and then went shopping. That afternoon, her sister borrowed the shoes and later put them under Sarah's bed. When Sarah gets ready, does she assume her shoes are under her dress?\n")
+
+    user_input = input("\nEnter your Scenario/Question: ")
     validator = InputValidator(user_input)
+
     if not validator.validate():
         return
 
@@ -228,7 +221,6 @@ def main():
 
     display_scenarios(scenarios)
 
-    # Create a new prompt with user_scenario, user_question, and the generated scenarios
     prompt = ""
     for i, scenario in enumerate(scenarios, start=1):
         prompt += f"Scenario {i}:\n\nScenario: \"{scenario['scenario']}\"\nQ: {scenario['question']}\nA: {scenario['answer']}\n\n"
@@ -239,9 +231,9 @@ def main():
 
     if answer is not None:
         processed_answer = process_answer(answer)
-        print(f"User Scenario: {user_scenario}\n\nQ: {user_question}\n\nA: {processed_answer}\n")
+        print(f"\nUser Scenario: {user_scenario}\n\nQ: {user_question}\n\nA: {processed_answer}\n")
     else:
-        print("No answer generated.")
+        print("Could not generate an answer. Please try again.")
 
 if __name__ == "__main__":
     main()
